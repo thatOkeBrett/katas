3a9b426268df5baca9c3df24644fa6a0f1f8098a
thatOkeBrett <moyobrett@gmail.com>
2020-04-12 23:26:42 +0200

    first completed tasks

---
+++ frame.js
@@ -0,0 +1,24 @@
+function Frame(arr) {
+    function fill (str, length, char) {
+        return (str.length < length) ? fill(str + char, length, char) : str;
+    }
+
+    let size = arr.map((str) => {
+            return str.length;
+})
+.reduce((a, b) => {
+        return Math.max(a, b);
+});
+
+    let line = fill('', size + 4, '*');
+
+    arr = arr.map((element) => {
+            return '* '+ fill(element, size, ' ') + ' *';
+})
+    arr.unshift(line);
+    arr.push(line);
+
+    return arr.join('\n');;
+}
+
+console.log(Frame(["Write", "good", "code", "every", "day"]));
