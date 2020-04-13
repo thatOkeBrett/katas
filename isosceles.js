d0e829362e7661d09b18aa378e85f62d9f5ff82f
thatOkeBrett <moyobrett@gmail.com>
2020-04-13 15:45:42 +0200

    done

---
+++ isosceles.js
@@ -0,0 +1,13 @@
+function isosceles(numOfLines){
+  //
+   for(i = 1; i<= numOfLines; i++){
+       for(k+ numOfLines; k>= i; k--){
+           console.log("")
+       }
+       for(j = 1; j <= i; j++){
+         console.log("#");
+         console.log("");  
+       }
+       consolelogIn()
+   } 
+}
